1745795fd3ea6ab10bc359dcf9768ebf39bbc2f1
sumoonyoko <718685292@qq.com>
2020-06-06 15:56:46 +0800

    【ADD】初始化项目

---
+++ src/main/java/top/summon/leetcode/LeetcodeApplication.java
@@ -0,0 +1,13 @@
+package top.summon.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeApplication.class, args);
+	}
+
+}
